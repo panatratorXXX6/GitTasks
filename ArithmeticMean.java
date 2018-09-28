1c3915037ad8230d4174f4d52a17230c6a354d00
panatratorxxx6 <you@example.com>
2018-09-28 10:57:28 +0300

    8

---
+++ ArithmeticMean.java
@@ -0,0 +1,13 @@
+package pack8;
+
+public class ArithmeticMean {
+    public static void main(String[] args) {
+
+        double arithmetMid;
+        double [] bubble = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+
+        arithmetMid = (bubble[0] + bubble[1] + bubble[2] + bubble[3] + bubble[4] + bubble[5])/6;
+
+        System.out.println(arithmetMid);
+    }
+}
